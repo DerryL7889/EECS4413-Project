f8a2d6619a9093171f56fbdbac7332cfe9c58578
IN2224 <irsa-nasir@outlook.com>
2023-03-18 12:46:22 -0400

    test commit

--- src/main/java/com/project/project/Application.java
+++ src/main/java/com/project/project/Application.java
@@ -3,6 +3,7 @@ package com.project.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test commit: Irsa
 @SpringBootApplication
 public class Application {
 
